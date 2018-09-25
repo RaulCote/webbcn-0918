07dcfb2d14505bdf67c024934736afb1bf797d8b
RaulCote <raulcote@hotmail.com>
2018-09-25 09:20:26 +0200

    Added buttons & link effects

--- students/raul.js
+++ students/raul.js
@@ -3,27 +3,27 @@
 function main() {
 
 
-  var perfilButton = document.querySelector('button.perfil-button')
+  var perfilButton = document.querySelector('button.intereses-button')
   var experimentsButton = document.querySelector('button.experiments-button')
-  var section = document.querySelector('section.section')
+  var section = document.querySelector('div.intereses')
   var sectionExp = document.querySelector('section.experiments')
 
 
   function hidePerfil(e) {
     section.classList.toggle('hidden-button-1')
     if (section.classList.contains('hidden-button-1')) {
-      e.currentTarget.innerText = 'Mostrar Perfil';
+      e.currentTarget.innerText = 'Intereses';
     } else {
-      e.currentTarget.innerText = 'Ocultar Perfil';
+      e.currentTarget.innerText = 'Ocultar Intereses';
     }
   }
 
   function hideExp(e) {
     sectionExp.classList.toggle('hidden-button-1')
     if (section.classList.contains('hidden-button-1')) {
-      e.currentTarget.innerText = 'Mostrar Perfil';
+      e.currentTarget.innerText = 'Experimentos';
     } else {
-      e.currentTarget.innerText = 'Ocultar Perfil';
+      e.currentTarget.innerText = 'Ocultar Experimentos';
     }
   }
 
